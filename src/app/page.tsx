f6f1b0eb3ed9c60cd4a5ff3242767a3c54e2dd7a
2fill <ichaeu@naver.com>
2025-07-03 19:32:37 +0900

    feat: sex 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
+  const [selected, setSelected] = useState<"male" | "female" | null>(null);
+
+  const handleClick = (gender: "male" | "female") => {
+    if(selected === gender){
+      setSelected(null);
+    }
+    else {
+      setSelected(gender);
+    }
+  };
+
   return (
     <div className="main">
       <div className="main-box">
         <img src="/Jibang-1.png" alt="Jibang1" className="Jibang-img-1" />
+
         <div className="order-box">
           <div className="order-item">
               <div className="circle-1">1</div>
@@ -20,6 +36,22 @@ export default function Page() {
               Body<br />
               composition
             </div>
+          </div>
+        </div>
+
+        <div className="sex-box">
+          <div className="sex-label">Sex</div>
+          <div className="sex-button">
+            <button
+              className={`sex-button-1 ${selected === "male" ? "selected" : ""}`}
+              onClick={() => handleClick("male")}>
+                Male
+            </button>
+            <button
+              className={`sex-button-2 ${selected === "female" ? "selected" : ""}`}
+              onClick={() => handleClick("female")}>
+                Female
+            </button>
           </div>
         </div>
       </div>
