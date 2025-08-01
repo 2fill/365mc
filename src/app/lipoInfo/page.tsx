966018f23c57b262ffa2ac3a53ed0086a30de1eb
2fill <ichaeu@naver.com>
2025-08-01 22:22:07 +0900

    feat: edema 추가

--- src/app/lipoInfo/page.tsx
+++ src/app/lipoInfo/page.tsx
@@ -4,11 +4,19 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from './lipoInfo.module.css';
 
+const edemaOptions = [
+    { label: 'No edema', className: 'edema-button-1' },
+    { label: 'Mild edema', className: 'edema-button-2' },
+    { label: 'Moderate to severe edema', className: 'edema-button-3' },
+] as const;
+type EdemaType = typeof edemaOptions[number]['label'] | null;
+
 export default function Page() {
     const router = useRouter();
 
     const [selectedType, setSelectedType] = useState<"Lams" | "Surgery" | null>(null);
     const [selectedSite, setSelectedSite] = useState<string | null>(null);
+    const [selectedEdema, setSelectedEdema] = useState<EdemaType>(null);
 
     const handleClick = (gender: "Lams" | "Surgery") => {
         if(selectedType === gender){
@@ -48,6 +56,8 @@ export default function Page() {
                 </div>
 
                 <div className={styles['form-box']}>
+
+                    {/* Liposuction type */}
                     <div className={styles['type-box']}>
                         <div className={styles['type-label']}>Liposuction Type</div>
                         <div className={styles['type-button']}>
@@ -67,6 +77,7 @@ export default function Page() {
                     {/* 구분선 */}
                     <div className={styles.divider}></div>
 
+                    {/* Liposuction site */}
                     <div className={styles['site-box']}>
                         <div className={styles['site-label']}>Liposuction Site</div>
                         <div className={styles['site-buttons']}>
@@ -88,17 +99,19 @@ export default function Page() {
                             ))}
                         </div>
                     </div>
-
+                    
+                    {/* 구분선 */}
                     <div className={styles.divider}></div>
 
                     <div className={styles['textbox-container']}>
+
                         {/* Preoperative Size */}
                         <div className={styles['input-box']}>
                             <div className={styles['input-label']}>Preoperative size</div>
                             <div className={styles['input-controls']}>
                                 <input
                                     type="number"
-                                    className={styles['input-textBox']}
+                                    className={`${styles['input-textBox']} ${preopSize !== null && preopSize !== 0 ? styles.focused : ''}`}
                                     value={preopSize !== null ? preopSize.toString() : ""}
                                     onChange={(e) => {
                                         let value = e.target.value;
@@ -122,7 +135,7 @@ export default function Page() {
                             <div className={styles['input-controls']}>
                                 <input
                                     type="number"
-                                    className={styles['input-textBox']}
+                                    className={`${styles['input-textBox']} ${fatVolume !== null && fatVolume !== 0 ? styles.focused : ''}`}
                                     value={fatVolume !== null ? fatVolume.toString() : ""}
                                     onChange={(e) => {
                                         let value = e.target.value;
@@ -136,8 +149,29 @@ export default function Page() {
                                     }}
                                 />
                                 <button className={styles['plus-button']} onClick={() => setFatVolume(prev => (prev ?? 0) + 1)}>+</button>
-                                <button className={styles['minus-button']} onClick={() => setPreopSize(prev => (prev ?? 0) > 0 ? (prev ?? 0) - 1 : 0)}>-</button>
+                                <button className={styles['minus-button']} onClick={() => setFatVolume(prev => (prev ?? 0) > 0 ? (prev ?? 0) - 1 : 0)}>-</button>
                             </div>
+                        </div>
+                    </div>
+
+                    {/* 구분선 */}
+                    <div className={styles.divider}></div>
+
+                    {/* Edema */}
+                    <div className={styles['edema-box']}>
+                        <div className={styles['edema-label']}>Edema</div>
+                        <div className={styles['edema-button']}>
+                            {edemaOptions.map(({label, className}) => (
+                                <button
+                                    key={label}
+                                    className={`${styles[className]} ${selectedEdema === label ? styles['selectedEdema'] : ''}`}
+                                    onClick={() => {
+                                        setSelectedEdema(selectedEdema === label ? null : label);
+                                    }}
+                                >
+                                    {label}
+                                </button>
+                            ))}
                         </div>
                     </div>
                 </div>
