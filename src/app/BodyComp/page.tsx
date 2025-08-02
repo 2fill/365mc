27b3b5ce938c01cff58d7e848441cfa34f536241
2fill <ichaeu@naver.com>
2025-08-02 23:59:10 +0900

    feat: height 추가

--- src/app/BodyComp/page.tsx
+++ src/app/BodyComp/page.tsx
@@ -14,30 +14,7 @@ type EdemaType = typeof edemaOptions[number]['label'] | null;
 export default function Page() {
     const router = useRouter();
 
-    const [selectedType, setSelectedType] = useState<"Lams" | "Surgery" | null>(null);
-    const [selectedSite, setSelectedSite] = useState<string | null>(null);
-    const [selectedEdema, setSelectedEdema] = useState<EdemaType>(null);
-
-    const handleClick = (gender: "Lams" | "Surgery") => {
-        if(selectedType === gender){
-            setSelectedType(null);
-        }
-        else {
-            setSelectedType(gender);
-        }
-    };
-
-    const [preopSize, setPreopSize] = useState<number | null>(null);
-    const [fatVolume, setFatVolume] = useState<number | null>(null);
-
-    const handleNextClick = () => {
-        if(selectedEdema){
-            router.push("/bodyComp");
-        }
-        else{
-            alert("Please fill in the required fields.");
-        }
-    }
+    const [height, setHeight] = useState<number | null>(null);
 
     return (
         <div className={styles.main}>
@@ -66,75 +43,29 @@ export default function Page() {
 
                 <div className={styles['form-box']}>
 
-                    {/* Liposuction type */}
-                    <div className={styles['type-box']}>
-                        <div className={styles['type-label']}>Liposuction Type</div>
-                        <div className={styles['type-button']}>
-                            <button
-                                className={`${styles['type-button-1']} ${selectedType === "Lams" ? styles.selectedType : ""}`}
-                                onClick={() => handleClick("Lams")}>
-                                Lams
-                            </button>
-                            <button
-                                className={`${styles['type-button-2']} ${selectedType === "Surgery" ? styles.selectedType : ""}`}
-                                onClick={() => handleClick("Surgery")}>
-                                Surgery
-                            </button>
-                        </div>
-                    </div>
+                    <div className={styles['first-container']}>
 
-                    {/* 구분선 */}
-                    <div className={styles.divider}></div>
-
-                    {/* Liposuction site */}
-                    <div className={styles['site-box']}>
-                        <div className={styles['site-label']}>Liposuction Site</div>
-                        <div className={styles['site-buttons']}>
-                            {['Abdomen', 'Arms', 'Backs', 'Buttocks', 'Calves', 'Flanks', 'Thighs'].map((site) => (
-                                <button
-                                    key={site}
-                                    className={`${styles['site-button']} ${selectedSite === site ? styles['selectedSite'] : ''}`}
-                                    onClick={() => {
-                                        if(selectedSite === site){
-                                            setSelectedSite(null);
-                                        }
-                                        else {
-                                            setSelectedSite(site);
-                                        }
-                                    }}
-                                >
-                                    {site}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                    
-                    {/* 구분선 */}
-                    <div className={styles.divider}></div>
-
-                    <div className={styles['textbox-container']}>
-
-                        {/* Preoperative Size */}
+                        {/* Height */}
                         <div className={styles['input-box']}>
-                            <div className={styles['input-label']}>Preoperative size</div>
+                            <div className={styles['input-label']}>Height</div>
                             <div className={styles['input-controls']}>
                                 <input
                                     type="number"
-                                    className={`${styles['input-textBox']} ${preopSize !== null && preopSize !== 0 ? styles.focused : ''}`}
-                                    value={preopSize !== null ? preopSize.toString() : ""}
+                                    className={`${styles['input-textBox']} ${height !== null && height !== 0 ? styles.focused : ''}`}
+                                    value={height !== null ? height.toString() : ""}
                                     onChange={(e) => {
                                         let value = e.target.value;
                                         if(value.length > 3) value = value.slice(0, 3);
                                         if(value.startsWith("0")) value = value.replace(/^0+/, "");
                                         const num = parseInt(value);
-                                        setPreopSize(isNaN(num) ? null : num);
+                                        setHeight(isNaN(num) ? null : num);
                                     }}
                                     onFocus={() => {
-                                        if(preopSize === null) setPreopSize(0);
+                                        if(height === null) setHeight(0);
                                     }}
                                 />
-                                <button className={styles['plus-button']} onClick={() => setPreopSize(prev => (prev ?? 0) + 1)}>+</button>
-                                <button className={styles['minus-button']} onClick={() => setPreopSize(prev => (prev ?? 0) > 0 ? (prev ?? 0) - 1 : 0)}>-</button>
+                                <button className={styles['plus-button']} onClick={() => setHeight(prev => (prev ?? 0) + 1)}>+</button>
+                                <button className={styles['minus-button']} onClick={() => setHeight(prev => (prev ?? 0) > 0 ? (prev ?? 0) - 1 : 0)}>-</button>
                             </div>
                         </div>
                     </div>
