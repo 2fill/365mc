eca35695294f41fbd67099314053f604176c9c44
2fill <ichaeu@naver.com>
2025-08-04 17:19:10 +0900

    fix: 404 오류 해결 시도

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    devIndicators : false
+    devIndicators: false,
+    appDir: true, 
 };
 
 export default nextConfig;
